26444853ed02052d1a9be3c88935c1fc543c3700
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2024-11-25 14:05:22 +0530

    fix: fixed custom view cell

--- LMChatUI_iOS/LMChatUI_iOS/Source/UI/MessageList/LMMessageCell/LMChatCustomCell.swift
+++ LMChatUI_iOS/LMChatUI_iOS/Source/UI/MessageList/LMMessageCell/LMChatCustomCell.swift
@@ -13,7 +13,42 @@ open class LMChatCustomCell: LMChatMessageCell {
         public var isSelected: Bool = false
     }
     
-    // MARK: configure
+    private let customMessageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is a custom view cell, modify it using LMUIComponent to generate your custom view"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .black
+        label.backgroundColor = .white
+        label.layer.cornerRadius = 8
+        label.layer.masksToBounds = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: Setup Views
+    open override func setupViews() {
+        super.setupViews()
+        // Add the custom message label to the cell's container view
+        contentView.addSubview(customMessageLabel)
+        chatMessageView.chatProfileImageView.isHidden = true
+        chatMessageView.usernameLabel.isHidden = true
+    }
+    
+    // MARK: Setup Layouts
+    open override func setupLayouts() {
+        super.setupLayouts()
+        // Add layout constraints for the custom message label
+        NSLayoutConstraint.activate([
+            customMessageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            customMessageLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            customMessageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            customMessageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    // MARK: Configure
     open func setData(with data: ContentModel, index: IndexPath) {
+        // You can customize this method to set additional data if needed
     }
 }
