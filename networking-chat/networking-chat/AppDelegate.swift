36d2e4f04bc806498ad5f9b3fa6c39d9056c444c
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2025-04-07 12:08:24 +0530

    feat: added separate themes for chat

---
+++ networking-chat/networking-chat/AppDelegate.swift
@@ -0,0 +1,116 @@
+//
+//  AppDelegate.swift
+//  networking-chat
+//
+//  Created by Anurag Tyagi on 04/04/25.
+//
+
+import FirebaseCore
+import FirebaseMessaging
+import LikeMindsChatCore
+import LikeMindsChatUI
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]?
+    ) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        registerForPushNotifications(application: application)
+        let deviceId = UIDevice.current.identifierForVendor?.uuidString
+        LMChatCore.shared.setupChat(deviceId: deviceId)
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+        print(error)
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        LMChatCore.shared.didReceieveNotification(
+            userInfo: response.notification.request.content.userInfo)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    private func registerForPushNotifications(application: UIApplication) {
+
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions
+        ) {
+            (granted, error) in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (
+            UNNotificationPresentationOptions
+        ) -> Void
+    ) {
+        LMChatCore.shared.willPresentNotification(
+            userInfo: notification.request.content.userInfo,
+            withCompletionHandler: completionHandler)
+    }
+
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(
+        _ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?
+    ) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+    }
+
+}
